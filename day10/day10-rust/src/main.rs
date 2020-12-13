38a363e6389569c6ceff7596b62fb441bcc371cc
wangy <yingge_gzb@qq.com>
2020-12-13 20:44:30 +0800

    day10

---
+++ day10/day10-rust/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let jolts = day10_rust::read_input("../input.txt");
+    println!("Part I: {}", day10_rust::part1_solution(&jolts));
+    println!("Part II: {}", day10_rust::part2_solution(&jolts));
+}
